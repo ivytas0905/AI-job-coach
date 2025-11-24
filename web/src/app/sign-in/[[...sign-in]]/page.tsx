e26cb4189329610ef596db3da04264410c744e32
Vita <vita@Vita-Dev.local>
2025-11-24 14:19:33 -0500

    Remove old web frontend directory

--- web/src/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { SignIn } from "@clerk/nextjs"
-
-export default function Page() {
-  return (<div className="flex justify-center p-10">
-       <SignIn />
-       </div>
-  )    
-}
